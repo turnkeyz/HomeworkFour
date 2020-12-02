69f99fb23115414a87fcd1dee1e21a283f0b4a36
turnkeyz <34150538+turnkeyz@users.noreply.github.com>
2020-12-01 19:27:12 -0600

    Add files via upload

---
+++ DescendingSort.py
@@ -0,0 +1,22 @@
+# TODO: Write a selection_sort_descend_trace() function that
+#       sorts the numbers list into descending order
+def selection_sort_descend_trace(numbers):
+    for i in range(len(numbers) - 1):
+        max_index = i
+        for j in range(i + 1, len(numbers)):
+            if numbers[max_index] < numbers[j]:
+                max_index = j
+        temp = numbers[i]
+        numbers[i] = numbers[max_index]
+        numbers[max_index] = temp
+        for num in range(len(numbers)):
+            print(numbers[num], end=' ')
+        print()
+
+
+if __name__ == "__main__":
+    # TODO: Read in a list of integers into numbers, then call
+    #       selection_sort_descend_trace() to sort the numbers
+    input_string = input()
+    numbers = input_string.split()
+    selection_sort_descend_trace(numbers)
---
+++ FatBurning.py
@@ -0,0 +1,22 @@
+def get_age():
+    age = int(input())
+    if age < 18 or age > 75:
+        raise ValueError('Invalid age.')
+    return age
+
+
+# TODO: Complete fat_burning_heart_rate() function
+def fat_burning_heart_rate(age):
+    heart_rate = .7 * (220 - age)
+    return heart_rate
+
+
+if __name__ == "__main__":
+    try:
+        age = get_age()
+        heart_rate = fat_burning_heart_rate(age)
+        print('Fat burning heart rate for a', age, 'year-old:', heart_rate, 'bpm')
+
+    except ValueError as exception:
+        print(exception)
+        print('Could not calculate heart rate info.\n')
